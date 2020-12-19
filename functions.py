f82cfa7f06b0c58aa00fa923fbe0e7effa84abc9
Moreira <jvitormoreirad@hotmail.com>
2020-12-18 22:12:18 -0300

    Add Changes to functions.py

--- functions.py
+++ functions.py
@@ -54,22 +54,61 @@ def atoms(formula):
     """
     pass
     # ======== YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return {formula}
+    if isinstance(formula, Not):
+        return atoms(formula.inner)
+    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        sub1 = atoms(formula.left)
+        sub2 = atoms(formula.right)
+        return (sub1).union(sub2)
 
 
 def number_of_atoms(formula):
     """Returns the number of distinct atoms occurring in a formula."""
     pass
     # ======== YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return 1
 
+    if isinstance(formula, Not):
+       return number_of_atoms(formula.inner)
+
+    if isinstance(formula, Not) or isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        return number_of_atoms(formula.left) + number_of_atoms(formula.right)
 
 def number_of_connectives(formula):
     """Returns the number of connectives occurring in a formula."""
     pass
     # ======== YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return 0
 
+    if isinstance(formula, Not):
+       return number_of_connectives(formula.inner) + 1
+
+    if isinstance(formula, Not) or isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        return number_of_connectives(formula.left) + number_of_connectives(formula.right) + 1
 
 def substitution(formula, old_subformula, new_subformula):
     """Returns a new formula obtained by replacing all occurrences
     of old_subformula in the input formula by new_subformula."""
     pass
     # ======== YOUR CODE HERE ========
+    if isinstance(formula, Atom) and (formula != old_subformula):
+        return formula
+
+    elif formula == old_subformula:
+        return new_subformula
+
+    elif isinstance(formula, Not) and (formula != old_subformula) :
+        return Not(substitution(formula.inner, old_subformula, new_subformula))
+
+    elif isinstance(formula, Implies) and (formula != old_subformula):
+        return Implies(substitution(formula.left,old_subformula,new_subformula), substitution(formula.right,old_subformula,new_subformula))
+
+    elif isinstance(formula, And) and (formula != old_subformula):
+        return And(substitution(formula.left, old_subformula, new_subformula), substitution(formula.right, old_subformula, new_subformula))
+
+    elif isinstance(formula, Or) and (formula != old_subformula):
+        return Or(substitution(formula.left, old_subformula, new_subformula), substitution(formula.right, old_subformula, new_subformula))
--- main.py
+++ main.py
@@ -15,31 +15,41 @@ formula7 = Implies(Not(And(Atom('p'), Atom('s'))), And(Atom('q'), Atom('r')))
 formula8 = Implies(Not(And(Atom('p'), Atom('s'))), And(Atom('q'), Not(And(Atom('p'), Atom('s')))))
 
 
-print('formula1:', formula1)
-print('formula2:', formula2)
-print('formula3:', formula3)
-print('formula4:', formula4)
-print('formula5:', formula5)
-print('formula6:', formula6)
-print('formula7:', formula7)
-print('formula8:', formula8)
+#print('formula1:', formula1)
+#print('formula2:', formula2)
+#print('formula3:', formula3)
+#print('formula4:', formula4)
+#print('formula5:', formula5)
+#print('formula6:', formula6)
+#print('formula7:', formula7)
+#print('formula8:', formula8)
 
-print('length of formula1:', length(formula1))
-print('length of formula3:', length(formula3))
+#print('length of formula1:', length(formula1))
+#print('length of formula3:', length(formula3))
 
-print('length of formula7:', length(formula7))
+#print('length of formula7:', length(formula7))
 
-print('subformulas of formula7:')
-print(subformulas(formula7))
-for subformula in subformulas(formula7):
-    print(subformula)
+#print('subformulas of formula7:')
+#print(subformulas(formula7))
+#for subformula in subformulas(formula7):
+#    print(subformula)
 
-print('length of formula8:', length(formula8))
-print('subformulas of formula8:')
-for subformula in subformulas(formula8):
-    print(subformula)
-
+#print('length of formula8:', length(formula8))
+#print('subformulas of formula8:')
+#print(formula8)
+print('===================================\n')
+#for subformula in subformulas(formula8):
+#   print(subformula)
+#print("===================================\n")
+#for atoms in atoms(formula8):
+#   print(atoms)
 #  we have shown in class that for all formula A, len(subformulas(A)) <= length(A):
 # for example, for formula8:
-print('number of subformulas of formula8:', len(subformulas(formula8)))
-print('len(subformulas(formula8)) <= length(formula8):', len(subformulas(formula8)) <= length(formula8))
+#print('number of subformulas of formula8:', len(subformulas(formula8)))
+#print('len(subformulas(formula8)) <= length(formula8):', len(subformulas(formula8)) <= length(formula8))
+formula_test = Implies(And(Atom('p'), Not(Atom('q'))), Atom('r'))
+#old_formula = Not(Atom('q'))
+#new_formula = Or(Atom('r'), Atom('t'))
+
+#print(substitution(formula_test,o,n))
+
